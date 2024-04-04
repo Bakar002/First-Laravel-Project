0796f6c45f65b40247f3adab848b3ec50a9e13bc
Bakar002 <abubakaramjad78600@gmail.com>
2024-04-05 02:48:08 +0500

    first route and controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,4 +7,25 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     //
+    public function index(){
+
+    }
+public function create(){
+
+return view("products.create");
+
+
+}
+
+        public function store(){}
+
+        public function edit(){
+
+        }
+        public function update(Request $request){}
+
+
+        public function destroy(Request $request){}
+
+
 }
